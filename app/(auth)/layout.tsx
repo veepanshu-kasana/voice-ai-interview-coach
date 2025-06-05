b080d70e1f91dcf8c54e4d66f78b7bd7753f6e81
veepanshu-kasana <veepanshukasana2728@gmail.com>
2025-06-05 15:31:23 +0530

    feat: Complete AI Mock Interview System (Next.js + Vapi + Firebase)

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+import { redirect } from 'next/navigation';
+
+import { isAuthenticated } from "@/lib/actions/auth.action"; //Needs to be done
+
+const AuthLayout = async ({children}: {children:ReactNode}) => {
+  const isUserAuthenticated = await isAuthenticated();
+  if (isUserAuthenticated) redirect("/");
+
+  return <div className='auth-layout'>{children}</div>
+}
+
+export default AuthLayout
